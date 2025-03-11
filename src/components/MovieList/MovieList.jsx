ef71a7cf3c060629f3f095b879e428a07c2c58aa
Ilgizik <ilgiz.ilishev@gmail.com>
2025-03-11 22:27:08 +0500

    added little layout 2

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,17 @@
 import "./MovieList.css";
 
 export default function MovieList() {
+  console.log(sdsdf);
+  var 1;
+  
   return (
+  
     <section className="movie-list">
       <div className="div1"></div>
       <div className="div2"></div>
-      <div className="div3"></div>
+          <div className="div3"></div>
+      
+      
       <div className="div4"></div>
       <div className="div5"></div>
       <div className="div6"></div>
